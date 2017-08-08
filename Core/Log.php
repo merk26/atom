21605651583a73f4c292082b28e9f599df5813fa
merk <merk@live.ru>
2017-08-08 18:04:53 +0700

    fix log file name

--- Core/Log.php
+++ Core/Log.php
@@ -16,7 +16,7 @@ class Log
 	static function F(): Logger\File
 	{
 		if (self::$_f == null)
-			self::$_f = new Logger\File($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "logs" . DIRECTORY_SEPARATOR . date('Y-m-d_') . ".psr3log");
+			self::$_f = new Logger\File($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "logs" . DIRECTORY_SEPARATOR . date('Y-m-d') . ".psr3.log");
 		return self::$_f;
 	}
 
